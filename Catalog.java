8b82055ab24639844ee526cef3a5f2944aeae817
stefandr99 <61435988+stefandr99@users.noreply.github.com>
2020-03-16 17:32:57 +0200

    Add files via upload

---
+++ Catalog.java
@@ -0,0 +1,51 @@
+package MyCatalog;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.String;
+
+public class Catalog implements Serializable {
+    private String name;
+    private String path;
+    private List<Document> documents = new ArrayList<>();
+
+    public Catalog(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<Document> documents) {
+        this.documents = documents;
+    }
+
+    public void add(Document doc) {
+        documents.add(doc);
+    }
+
+    public Document findById(String id) {        
+        return documents.stream()
+                .filter(d -> d.getId().equals(id)).findFirst().orElse(null);
+    }
+
+}
---
+++ Document.java
@@ -0,0 +1,45 @@
+package MyCatalog;
+
+import javafx.util.Pair;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Document implements Serializable {
+    String id;
+    String name;
+    String location;
+    private Map<String, Object> tags = new HashMap<>();
+
+
+    public Document() {
+    }
+
+    public Document(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addTag(String key, Object obj) {
+        tags.put(key, obj);
+    }
+}
